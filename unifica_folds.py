a3f991d243e24673ce967b48416d150de4bbb941
unknown <mangussiarthur@gmail.com>
2024-10-02 22:37:17 -0300

    Resultados XGB

---
+++ unifica_folds.py
@@ -0,0 +1,62 @@
+import os 
+import pandas as pd
+
+
+for type_attack in ["evasion", "poison"]:
+
+  for model_impt in ["gain",
+                        "knn",
+                        "mice",
+                        "softimpute"]:
+      for mecanismo in ["MAR-correlated_Multivariado",
+                        "MNAR-determisticTrue_Multivariado"]:
+        
+        os.makedirs(f"./Attacks/{type_attack}/{model_impt}/DatasetsUnificados/{mecanismo}", exist_ok=True)
+
+        for name_dataset in ["acute-inflammations",
+                  "autism-adolescent",
+                  "autism-adult",
+                  "autism-child",
+                  "bc-coimbra",
+                  "blood-transfusion",
+                  "contraceptive-methods",
+                  "diabetic",
+                  "echocardiogram",
+                  "fertility",
+                  "german-credit",
+                  "haberman",
+                  "hcv-egyptian",
+                  "heart-cleveland",
+                  "hepatitis",
+                  "iris",
+                  "liver",                                              
+                  "mathernal-risk",
+                  "npha",
+                  "parkinsons",
+                  "phoneme",
+                  "pima-diabetes",
+                  "proba_football",
+                  "ricci",
+                  "sa-heart",
+                  "thoracic-surgery",
+                  "thyroid-recurrence",
+                  "wine",
+                  "wiscosin"
+                        ]:
+          
+          for mr in [5,20,40]:
+
+              folds = []
+              for fold in range (5):
+                
+                path = f"./Attacks/{type_attack}/{model_impt}/Datasets/{mecanismo}"
+                arq = f"{name_dataset}_{model_impt}_fold{fold}_md{mr}.csv"
+                    
+                df = pd.read_csv(os.path.join(path,arq))
+                folds.append(df)
+                    
+              df_unificado = pd.concat(folds, ignore_index=True)
+              
+              df_unificado.to_csv(f"./Attacks/{type_attack}/{model_impt}/DatasetsUnificados/{mecanismo}/{name_dataset}_{model_impt}_md{mr}.csv", index=False)        
+
+print("done")
